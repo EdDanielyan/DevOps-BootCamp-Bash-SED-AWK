c12fa99e5bfcdd20e59fa8f9cbb3054f6bb2e252
EdDanielyan <102631130+EdDanielyan@users.noreply.github.com>
2023-01-12 15:30:51 +0400

    Update sedawk.sh

--- sedawk.sh
+++ sedawk.sh
@@ -4,10 +4,12 @@
 
 cp /home/ubuntu/passwd /home/ubuntu/passwd_new
 
-awk -F: '$1 == "saned" {$7="/bin/bash"} 1' /home/ubuntu/passwd_new OFS=":" passwd_new
+awk '/saned/ {sub(/usr\/sbin\/nologin/, "bin/bash")} 1' passwd > passwd_new
 
-sed -i '/avahi/s|/usr/sbin/nologin|/bin/csh|g' /home/ubuntu/passwd_new
+sed -i '/avahi/ s/usr\/sbin\/nologin/bin\/bash/' passwd_new
 
-awk -F: '{print $1,$3,$5,$7}' OFS=":" /home/ubuntu/passwd_new > /home/ubuntu/passwd_new2
+awk -F: '{print $1,$3,$5,$7}' OFS=":" passwd_new > passwd_new2 && mv passwd_new2 passwd_new
 
 sed -i '/daemon/d' passwd_new 
+
+awk -F: 'BEGIN {OFS=":"} $2 %2 == 0 {$4 = "/bin/zsh"} { print }' passwd_new > passwd_new2 && mv passwd_new2 passwd_new
